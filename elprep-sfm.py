0d42a090a13811753af40b863af839a7eddff3bb
Charlotte Herzeel <charlotte.herzeel@imec.be>
2014-10-31 13:47:08 +0100

    Example script for using split/merge tools in a pipeline.

---
+++ elprep-sfm.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+# Example script for using elPrep split/merge tools.  
+
+import sys
+import subprocess
+import os
+import time
+
+# utilities
+
+def member(el,l):
+  i = iter(l)
+  for val in i:
+    if val == el:
+      return list(i)
+
+def cmd_option(opt, cmdl):
+  val = member(opt, cmdl)
+  return [opt,val[0]] if val else []
+
+# actual script
+
+def elprep_sfm ():
+  # set up directories for intermediate results
+  file_in = sys.argv[1]
+  file_out = sys.argv[2]
+  input = os.path.basename(file_in)
+  output_prefix, output_extension = os.path.splitext(input) 
+  stamp = str(time.time())
+  split_dir = os.path.join(os.getcwd(), "temp-" + stamp + os.sep)
+  result_dir = os.path.join(os.getcwd(), "temp-processed-" + stamp + os.sep)  
+  os.mkdir(split_dir)
+  os.mkdir(result_dir)
+  # split command
+  nr_of_threads_opt = cmd_option("--nr-of-threads", sys.argv)
+  subprocess.call(["elprep", "split", file_in, split_dir] + nr_of_threads_opt) 
+  # run filter command for split files
+  for root, dirs, files in os.walk(split_dir):
+    for file in files:
+      ffile = os.path.join(root, file)
+      processed_file = os.path.join(result_dir, os.path.basename(file))
+      print ["elprep", ffile, processed_file] + sys.argv[3:]
+      subprocess.call(["elprep", ffile, processed_file] + sys.argv[3:]) 
+      os.remove(ffile)
+  os.rmdir(split_dir)
+  # merge command
+  subprocess.call(["elprep", "merge", result_dir, file_out] + nr_of_threads_opt)
+  # remove directories for intermediate results
+  for root, dirs, files in os.walk(result_dir):
+    for file in files:
+      ffile = os.path.join(root, file)
+      os.remove(ffile)
+  os.rmdir(result_dir) 
+
+elprep_sfm()
+
